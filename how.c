8dd4d49fd9f505bfe8d85ca6cc624e8f9376a230
studynotlimit <lillard20212021@163.com>
2022-10-15 03:34:22 +0800

    Update how.c

--- how.c
+++ how.c
@@ -1,41 +1,129 @@
-void f1(char *rev_buf,char *msg){
+void recv_user_sign_up(char *rev_buf,char *msg){
     int tmp;
     memcpy(&tmp,recv_buf,sizeof(tmp));
     switch (tmp){
         case OK:
-        sprintf(msg,"OK");
+        sprintf(msg,"恭喜你，注册成功！");
+        break;
+        case ALREADY_EXITS:
+        sprintf(msg,"账号已经存在");
         break;
         case NOT_OK:
-        sprintf(msg,"NOT_OK");
+        sprintf(msg,"不好意思，服务器出错了。");
         break;
     }
     display_info(msg);
 }
-void f2(char *rev_buf,char *msg){
+void recv_user_sign_in(char *rev_buf,char *msg){
     int tmp;
     memcpy(&tmp,recv_buf,sizeof(tmp));
     switch (tmp){
         case OK:
-        sprintf(msg,"OK");
+        sprintf(msg,"恭喜你，登录成功！");
         break;
         case NOT_EXITS:
-        sprintf(msg,"NOT_EXITS");
+        sprintf(msg,"不好意思，账号不存在。");
         break;
         case WRONG_PASSWORD:
-        sprintf(msg,"WRONG_PASSWORD");
+        sprintf(msg,"不好意思，密码错误");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
         break;
     }
     display_info(msg);
 }
-void f3(char *rev_buf,char *msg){
+void recv_user_change_pass(char *rev_buf,char *msg){
     int tmp;
     memcpy(&tmp,recv_buf,sizeof(tmp));
     switch (tmp){
         case OK:
-        sprintf(msg,"OK");
+        sprintf(msg,"恭喜你，修改密码成功！");
         break;
         case NOT_EXITS:
-        sprintf(msg,"NOT_EXITS");
+        sprintf(msg,"不好意思，账号不存在");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
+        break;
+    }
+    display_info(msg);
+}
+
+void recv_manger_sign_up(char *rev_buf,char *msg){
+    int tmp;
+    memcpy(&tmp,recv_buf,sizeof(tmp));
+    switch (tmp){
+        case OK:
+        sprintf(msg,"恭喜你，注册成功！");
+        break;
+        case ALREADY_EXITS:
+        sprintf(msg,"账号已经存在");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
+        break;
+    }
+    display_info(msg);
+}
+void recv_manger_sign_in(char *rev_buf,char *msg){
+    int tmp;
+    memcpy(&tmp,recv_buf,sizeof(tmp));
+    switch (tmp){
+        case OK:
+        sprintf(msg,"恭喜你，登录成功！");
+        break;
+        case NOT_EXITS:
+        sprintf(msg,"不好意思，账号不存在。");
+        break;
+        case WRONG_PASSWORD:
+        sprintf(msg,"不好意思，密码错误");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
+        break;
+    }
+    display_info(msg);
+}
+void recv_manger_change_pass(char *rev_buf,char *msg){
+    int tmp;
+    memcpy(&tmp,recv_buf,sizeof(tmp));
+    switch (tmp){
+        case OK:
+        sprintf(msg,"恭喜你，修改密码成功！");
+        break;
+        case NOT_EXITS:
+        sprintf(msg,"不好意思，账号不存在");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
+        break;
+    }
+    display_info(msg);
+}
+
+void recv_get_all_exp(char *rev_buf,char *msg){
+    int tmp;
+    memcpy(&tmp,recv_buf,sizeof(tmp));
+    switch (tmp){
+        case OK:
+        sprintf(msg,"得到所有快递信息！");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
+        break;
+    }
+    display_info(msg);
+}
+void recv_add_single_exp(char *rev_buf,char *msg){
+    int tmp;
+    memcpy(&tmp,recv_buf,sizeof(tmp));
+    switch (tmp){
+        case OK:
+        sprintf(msg,"添加快递成功！");
+        break;
+        case NOT_OK:
+        sprintf(msg,"不好意思，服务器出错了。");
         break;
     }
     display_info(msg);
